bbca52a75ee812acc86dd64b7177c073c8623a6f
Yağmur K. <yagmurrttk@gmail.com>
2024-02-27 14:43:41 +0300

    Lesson 17

---
+++ BusinessLayer/Concrete/FeatureManager.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstact;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class FeatureManager : IFeatureDal
+    {
+        IFeatureDal _featureDal;
+
+        public FeatureManager(IFeatureDal featureDal)
+        {
+            _featureDal = featureDal;
+        }
+
+        public void Delete(Feature t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Feature> GetList()
+        {
+            return _featureDal.GetList();
+        }
+
+        public void Insert(Feature t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Feature t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TraversalCoreProject/ViewComponents/Default/_Feature.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _Feature : ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = featureManager.GetList();
+            return View(values);
+        }
+    }
+}
